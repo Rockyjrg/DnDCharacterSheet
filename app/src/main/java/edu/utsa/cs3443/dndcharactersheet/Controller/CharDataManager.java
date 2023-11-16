33d21dd799d5cc92a65022a79f1cc0196bdae439
Dynadinomo <sifuentesgael2003@gmail.com>
2023-11-16 12:38:20 -0600

    Made the character class using two classes, Attributes and Statistics.java and combined them using Character.java, you would have to declare Attributes.java and Statistics.java to declare Character.java. Please look through each of them at your own discression Also I added a view for stat edits and a view to look at char Added Loder and saver classes as well as data class, have yet to implement functionality

---
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Controller/CharDataManager.java
@@ -0,0 +1,37 @@
+package edu.utsa.cs3443.dndcharactersheet.Controller;
+
+import android.app.Activity;
+
+import edu.utsa.cs3443.dndcharactersheet.Model.Attributes;
+import edu.utsa.cs3443.dndcharactersheet.Model.Character;
+import edu.utsa.cs3443.dndcharactersheet.Model.Statistics;
+
+public class CharDataManager {
+
+    public Activity activity;
+
+    public CharDataManager(Activity activity){
+        this.activity = activity;
+    }
+
+    public Character loadCharacter(int saveFile){
+        Character temp;
+        Attributes tempAtt;
+        Statistics tempStats;
+
+
+
+        return null;
+
+    }
+
+    public void saveCharacter(int saveFile, Character chr){
+        Character temp;
+        Attributes tempAtt;
+        Statistics tempStats;
+
+
+
+    }
+
+}
