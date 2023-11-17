92a09ba7356501767ff9e1cb1855f239b6bc8093
Brandon Ariel <brandonariel59@gmail.com>
2023-11-17 13:32:12 -0600

    Started MainActivity and MainController

--- app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Controller/MainController.java
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Controller/MainController.java
@@ -1,4 +1,22 @@
 package edu.utsa.cs3443.dndcharactersheet.Controller;
 
-public class MainController {
+import android.content.res.AssetManager;
+import android.view.View;
+
+import edu.utsa.cs3443.dndcharactersheet.MainActivity;
+
+public class MainController implements View.OnClickListener {
+
+    MainActivity activity;
+
+    AssetManager assets;
+    public MainController(MainActivity activity) {
+        this.activity = activity;
+        assets = activity.getAssets();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
--- app/src/main/java/edu/utsa/cs3443/dndcharactersheet/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/MainActivity.java
@@ -5,6 +5,9 @@ package edu.utsa.cs3443.dndcharactersheet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import edu.utsa.cs3443.dndcharactersheet.Controller.MainController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainController controller1 = new MainController(this);
+
+        Button edit1 = findViewById(R.id.editButton);
+        edit1.setOnClickListener(controller1);
+
+        Button delete1 = findViewById(R.id.deleteButton);
+        delete1.setOnClickListener(controller1);
+
+        Button edit2 = findViewById(R.id.editButton2);
+        edit2.setOnClickListener(controller1);
+
+        Button delete2 = findViewById(R.id.deleteButton2);
+        delete2.setOnClickListener(controller1);
+
+        Button edit3 = findViewById(R.id.editButton3);
+        edit3.setOnClickListener(controller1);
+
+        Button delete3 = findViewById(R.id.deleteButton3);
+        delete3.setOnClickListener(controller1);
+
+        Button edit4 = findViewById(R.id.editButton4);
+        edit4.setOnClickListener(controller1);
+
+        Button delete4 = findViewById(R.id.deleteButton4);
+        delete4.setOnClickListener(controller1);
+
+        Button edit5 = findViewById(R.id.editButton5);
+        edit5.setOnClickListener(controller1);
+
+        Button delete5 = findViewById(R.id.deleteButton5);
+        delete5.setOnClickListener(controller1);
+
+
+
     }
 }
