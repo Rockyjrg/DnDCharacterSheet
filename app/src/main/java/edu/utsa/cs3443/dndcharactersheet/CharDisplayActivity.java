6d37e0edf47eb16f26bb43dccaae05e284350528
Brandon Ariel <brandonariel59@gmail.com>
2023-11-22 22:06:40 -0600

    Updated CharDisplayActivity

--- app/src/main/java/edu/utsa/cs3443/dndcharactersheet/CharDisplayActivity.java
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/CharDisplayActivity.java
@@ -1,13 +1,25 @@
 package edu.utsa.cs3443.dndcharactersheet;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import edu.utsa.cs3443.dndcharactersheet.Controller.CharDisplayController;
 
 public class CharDisplayActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_char_base_stats_display);
+
+        CharDisplayController charDisplayController = new CharDisplayController(this);
+
+        Button spell = findViewById(R.id.spells);
+        spell.setOnClickListener(charDisplayController);
+        Button weapons = findViewById(R.id.weapons);
+        weapons.setOnClickListener(charDisplayController);
+
+
     }
 }
