0537e0e87eddbb1d81064a31b2db5247e6a5d91b
Israe <Israe@LAPTOP-VS4RO1RM>
2023-11-22 22:00:52 -0600

    created a bunch of activities and other stuff, also, finshed your mom

---
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/CharSpellsDisplayActivity.java
@@ -0,0 +1,13 @@
+package edu.utsa.cs3443.dndcharactersheet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CharSpellsDisplayActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_spells_display);
+    }
+}
--- app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Controller/CharDisplayController.java
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Controller/CharDisplayController.java
@@ -1,4 +1,32 @@
 package edu.utsa.cs3443.dndcharactersheet.Controller;
 
-public class CharDisplayController {
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.view.View;
+
+import edu.utsa.cs3443.dndcharactersheet.CharDisplayActivity;
+import edu.utsa.cs3443.dndcharactersheet.MainActivity;
+import edu.utsa.cs3443.dndcharactersheet.R;
+
+public class CharDisplayController implements View.OnClickListener {
+
+    CharDisplayActivity activity;
+
+    AssetManager assets;
+    public CharDisplayController(CharDisplayActivity activity) {
+        this.activity = activity;
+        assets = activity.getAssets();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int id = v.getId();
+
+        if(id == R.id.spells) {
+            Intent intent = new Intent(activity, CharDisplayActivity.class);
+            activity.startActivity(intent);
+
+        }
+    }
 }
