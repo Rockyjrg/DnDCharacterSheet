66b1430a99f662b4da17cf47a8912e84eb744778
Rockyjrg <jamie.g.ii@hotmail.com>
2023-11-22 18:05:09 -0600

    Edited the Character.java class -Commented out previous variable names -Added Strings such as name, race, characterClass, statistics, and attributes

--- app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Model/Character.java
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Model/Character.java
@@ -9,17 +9,67 @@ import edu.utsa.cs3443.dndcharactersheet.Model.Attributes;
 import edu.utsa.cs3443.dndcharactersheet.Model.Statistics;
 
 public class Character {
-    public Statistics stats;
-    public Attributes atts;
 
-    public Character(Statistics stats,Attributes atts) {
-        this.stats = stats;
-        this.atts = atts;
+    private String name;
+    private String race;
+    private String characterClass;
+    private String statistics;
+    private String attributes;
+//    public Statistics stats;
+//    public Attributes atts;
+
+    public Character(String name, String race, String characterClass, String statistics, String attributes) {
+        this.name = name;
+        this.race = race;
+        this.characterClass = characterClass;
+        this.statistics = statistics;
+        this.attributes = attributes;
     }
 
-    @Override
-    public String toString() {
-        return atts.toString() + stats.toString();
+//    @Override
+//    public String toString() {
+//        return atts.toString() + stats.toString();
+//    }
+//
+
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+
+    public String getStatistics() {
+        return statistics;
+    }
+
+    public void setStatistics(String statistics) {
+        this.statistics = statistics;
+    }
+
+    public String getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(String attributes) {
+        this.attributes = attributes;
+    }
 }
