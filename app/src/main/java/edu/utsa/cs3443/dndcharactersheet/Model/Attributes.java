1e0f0266f64364efc95d8909d6bc6631d0b6dcb5
Israe <Israe@LAPTOP-VS4RO1RM>
2023-11-22 20:52:31 -0600

    finshed the spell & cantrips sheet for viewing

--- app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Model/Attributes.java
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Model/Attributes.java
@@ -6,9 +6,10 @@ package edu.utsa.cs3443.dndcharactersheet.Model;
 
 public class Attributes {
     //Initializes the Name field, as well as the Armor Class and Speed statistics
+
+    //changed variables declared from "public String name; public String role;" -> what it is now
     public String name, role;
-    public int ac; public int spd;
-    public int hp; public int initBonus;
+    public int ac, spd, hp, initBonus;
 
     //------------------------------------------------------------------------------------------------------------
 
