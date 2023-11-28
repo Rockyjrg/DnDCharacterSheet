c10caf3abf935da6537498c886f55cdf0ea81de0
Dynadinomo <sifuentesgael2003@gmail.com>
2023-11-28 15:23:38 -0600

    Pushing spells and Wepons object classes

--- app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Model/Wepons.java
+++ app/src/main/java/edu/utsa/cs3443/dndcharactersheet/Model/Wepons.java
@@ -1,2 +1,37 @@
-package edu.utsa.cs3443.dndcharactersheet.Model;public class Wepons {
+package edu.utsa.cs3443.dndcharactersheet.Model;
+
+public class Wepons {
+    String name;
+    int attackBonus;
+    String DamageType;
+
+    public Wepons(String name, int attackBonus, String damageType) {
+        this.name = name;
+        this.attackBonus = attackBonus;
+        DamageType = damageType;
+    }
+
+    public String getName()
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttackBonus() {
+        return attackBonus;
+    }
+
+    public void setAttackBonus(int attackBonus) {
+        this.attackBonus = attackBonus;
+    }
+
+    public String getDamageType() {
+        return DamageType;
+    }
+
+    public void setDamageType(String damageType) {
+        DamageType = damageType;
+    }
 }
